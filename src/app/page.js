0f38469d7e9a6bdedd331cf44faae45e0ce18220
gregorb <g@bokal.net>
2026-07-20 18:51:52 +0200

    Statična koda QR

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+'use client';
+
+import { QRCodeCanvas } from 'qrcode.react';
+
 export default function Home() {
   return (
     <main className="max-w-md mx-auto my-10 p-6">
       <h1 className="text-2xl font-bold mb-5">Generator</h1>
       <p className="text-grey-600">Juhuhu!</p>
+      <QRCodeCanvas value="https://veleum.pro" size={200}/>
     </main>
   )
 }
