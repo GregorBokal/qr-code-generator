27a72ffcf0f67c01d4443eabb2dad6fb8c82a79a
gregorb <g@bokal.net>
2026-07-20 19:01:28 +0200

    Vnos URLja.

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
 'use client';
 
+import { useState } from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
 
 export default function Home() {
+  
+  const [text, setText] = useState('https://veleum.pro')
+
   return (
     <main className="max-w-md mx-auto my-10 p-6">
       <h1 className="text-2xl font-bold mb-5">Generator</h1>
       <p className="text-grey-600">Juhuhu!</p>
-      <QRCodeCanvas value="https://veleum.pro" size={200}/>
+      <label className="block mb-4">
+       <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="URL"
+        />
+      </label>
+      <div>
+        <QRCodeCanvas value={text} size={200}/>
+      </div>
     </main>
   )
 }
