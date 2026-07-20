e86d75f7ea78c0b8272df36cfdd0584b23691ebe
gregorb <g@bokal.net>
2026-07-20 19:06:09 +0200

    CSS

--- src/app/page.js
+++ src/app/page.js
@@ -10,12 +10,18 @@ export default function Home() {
   return (
     <main className="max-w-md mx-auto my-10 p-6">
       <h1 className="text-2xl font-bold mb-5">Generator</h1>
-      <p className="text-grey-600">Juhuhu!</p>
       <label className="block mb-4">
        <input
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="URL"
+        className="
+          rounded-lg 
+          border border-gray-300 px-3 py-2
+          focus:border-blue-500 
+          focus:ring-2
+          focus:ring-blue-500
+          focus:outline-none"
         />
       </label>
       <div>
